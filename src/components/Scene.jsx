87840e56ffe8b3113979054161d9f8379c67bd14
Sangjin Hwang <aosdb5640@naver.com>
2025-04-15 22:59:23 +0900

    add: import naver 3d model

--- src/components/Scene.jsx
+++ src/components/Scene.jsx
@@ -1,14 +1,14 @@
 import { OrbitControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import { BoxGeometry, MeshBasicMaterial } from "three";
+import NaverBuilding from "./model/NaverBuilding";
 
 const Scene = () => {
   return (
     <Canvas>
-      <mesh>
-        <boxGeometry />
-        <meshBasicMaterial />
-      </mesh>
+      <ambientLight intensity={5} color="#ffffff" />
+
+      <NaverBuilding />
       <OrbitControls />
     </Canvas>
   );
---
+++ src/components/model/NaverBuilding.jsx
@@ -0,0 +1,20 @@
+import React, { useRef } from "react";
+import { useGLTF } from "@react-three/drei";
+
+export default function NaverBuilding(props) {
+  const { nodes, materials } = useGLTF("/NaverLabs1784.glb");
+  return (
+    <group {...props} dispose={null}>
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.Cube015.geometry}
+        material={nodes.Cube015.material}
+        position={[10.358, 0.094, 13.891]}
+        scale={0.25}
+      />
+    </group>
+  );
+}
+
+useGLTF.preload("/NaverLabs1784.glb");
