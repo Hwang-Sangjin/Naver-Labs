08faceb91c4dacb54b84f3398faf7edb4f329a88
Sangjin Hwang <aosdb5640@naver.com>
2025-07-24 21:48:29 +0900

    add: shader with vignette

--- src/components/Vignette.jsx
+++ src/components/Vignette.jsx
@@ -1,8 +1,31 @@
+import vignetteVertexShader from "../shaders/vignette/vertex.glsl";
+import vignetteFragmentShader from "../shaders/vignette/fragment.glsl";
+import { useMemo } from "react";
+import * as THREE from "three";
+
 const Vignette = () => {
+  const uniform = useMemo(
+    () => ({
+      uVignetteColor: { value: new THREE.Color("#4f1f96") }, // THREE.Color 사용 권장
+      uVignetteMultiplier: { value: 1.16 },
+      uVignetteOffset: { value: -1 },
+      uOverlayColor: { value: new THREE.Color("#130621") }, // THREE.Color 사용 권장
+      uOverlayAlpha: { value: 1 },
+    }),
+    []
+  );
+
   return (
     <mesh>
       <planeGeometry args={[40, 30, 10, 10]} />
-      <meshBasicMaterial />
+      <shaderMaterial
+        attach="material"
+        //uniform={uniform}
+        vertexShader={vignetteVertexShader}
+        fragmentShader={vignetteFragmentShader}
+        transparent={true}
+        depthTest={false}
+      />
     </mesh>
   );
 };
