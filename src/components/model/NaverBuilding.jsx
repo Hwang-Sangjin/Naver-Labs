e378ead0a8a15a3566c47f318fffcf26df8ced04
Sangjin Hwang <aosdb5640@naver.com>
2025-08-21 23:39:02 +0900

    del: quote

--- src/components/model/NaverBuilding.jsx
+++ src/components/model/NaverBuilding.jsx
@@ -5,22 +5,6 @@ import { useFrame } from "@react-three/fiber";
 export default function NaverBuilding(props) {
   const { nodes, materials } = useGLTF("/model/NaverLabs1784_001.glb");
 
-  // const scroll = useScroll();
-
-  // useFrame((state, delta) => {
-  //   const offset = 1 - scroll.offset;
-
-  //   state.camera.position.set(
-  //     Math.sin(offset) * 100 + 30,
-  //     Math.atan(offset * Math.PI * 2) + 100,
-  //     Math.cos((offset * Math.PI) / 3) * 200 + 100
-  //   );
-  //   state.camera.zoom = 10 + (1 - offset) * 5; // Zoom ranges from 5 to 15 based on scroll
-  //   state.camera.updateProjectionMatrix();
-
-  //   state.camera.lookAt(-2, 2, 0);
-  // });
-
   return (
     <Center>
       <OrbitControls />
